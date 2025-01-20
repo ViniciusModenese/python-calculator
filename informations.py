9eb6d87ffc9120e6744988f153024c4a43d7e4a8
Vinicius Modenese <modenesevini@gmail.com>
2025-01-20 15:11:15 -0300

    Add style/theme
    Add the qdarkstyle theme and additional QSS configuration

---
+++ informations.py
@@ -0,0 +1,13 @@
+from PySide6.QtWidgets import QLabel
+from PySide6.QtCore import Qt
+from variables import MIN_FONT_SIZE
+
+
+class Info(QLabel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cfg_style()
+
+    def cfg_style(self):
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setStyleSheet(f'font-size: {MIN_FONT_SIZE}px')
